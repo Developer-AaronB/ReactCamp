728f4c8f396321e1ab9d951a78608d1bdcefb179
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-13 20:39:19 -0500

    Task 1 complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
+import CampsiteCard from "./componets/CampsiteCard.js";
+import { CAMPSITES } from "./app/shared/CAMPSITES";
 
 function App() {
   return (
     <div className="App">
-        <Navbar dark color='primary' sticky='top' expand='md'>
-          <Container>
-            <NavbarBrand href='/'>
-              <img src={NucampLogo} alt='nucamp logo' />
-            </NavbarBrand>
-          </Container>
-        </Navbar>
-        I'm ready for workshop!
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      <CampsiteCard campsite={CAMPSITES[0]} />
     </div>
   );
 }
