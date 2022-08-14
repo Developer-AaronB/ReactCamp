827647d22581e2ac67a0ee19f3f0730371b1bb47
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-13 21:49:28 -0500

    Complete workshop 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import "./App.css";
 import CampsitesList from "./features/campsites/CampsitesList";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <CampsitesList />
+      <Footer />
     </div>
   );
 }
