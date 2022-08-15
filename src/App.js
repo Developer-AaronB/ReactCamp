cf6e353ea781e4d814370f9f9771e87c9906fae2
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-15 18:22:43 -0500

    CampsitesDirectoryPage & Campsite Detail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
-import "./App.css";
-import CampsitesList from "./features/campsites/CampsitesList";
 import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./features/campsites/CampsitesList";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
