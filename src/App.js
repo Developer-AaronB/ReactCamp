b2e3562682c51a9339b63b038dd50781ddaa1d8d
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-17 21:59:35 -0500

    Practice with Andrew

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import "./App.css";
-
-
+import Quantity from "./components/quantity";
 
 function App() {
   return (
