88d0c8f5462dd5ea07ea690ad87b0ad6cea72797
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-24 01:45:50 -0500

    Tried to import and see if I can get it to work

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -3,16 +3,16 @@ import { useParams } from "react-router-dom";
 import { selectCampsiteBySlug } from "../features/campsites/campsitesSlice";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CommentsList from '../features/comments/CommentsList';
-
+import { selectCommentsByCampsiteId } from "../features/comments/commentsSlice";
 const CampsiteDetailPage = () => {
   const { slug, campsiteId } = useParams();
   const campsite = selectCampsiteBySlug(slug);
-
+  const campsiteComments = selectCommentsByCampsiteId(campsiteId)
   return (
     <Container>
       <Row>
         <CampsiteDetail campsite={campsite} />
-        <CommentsList campsiteId={campsiteId} />
+        <CommentsList campsiteId={campsiteComments} />
       </Row>
     </Container>
   );
