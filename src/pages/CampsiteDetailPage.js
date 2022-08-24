1926348dcf83fa6c08dde14b8bef89728b0e75d9
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-24 01:42:27 -0500

    Problem with Comments
    I could not get the comments page to show by the campsiteId. I think something happened with the detail page and slice

--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -2,15 +2,17 @@ import { Container, Row } from "reactstrap";
 import { useParams } from "react-router-dom";
 import { selectCampsiteBySlug } from "../features/campsites/campsitesSlice";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import CommentsList from '../features/comments/CommentsList';
 
 const CampsiteDetailPage = () => {
-  const { slug } = useParams();
+  const { slug, campsiteId } = useParams();
   const campsite = selectCampsiteBySlug(slug);
 
   return (
     <Container>
       <Row>
         <CampsiteDetail campsite={campsite} />
+        <CommentsList campsiteId={campsiteId} />
       </Row>
     </Container>
   );
