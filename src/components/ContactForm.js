37cf68b7e31cf51e21107369490764b3f9a26269
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-08-28 23:35:10 -0500

    Update Contact Form
    end of part 2

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,51 +16,55 @@ const ContactForm = () => {
       >
         <Form>
           <FormGroup row>
-            <Label htmlFor='firstName' md='2'>
-                First Name
+            <Label htmlFor="firstName" md="2">
+              First Name
             </Label>
-            <Col md='10'>
-            
+            <Col md="10">
+              <Field name='firstName' placeholder='First Name' className="form-control" />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label htmlFor='lastName' md='2'>
-                Last Name
+            <Label htmlFor="lastName" md="2">
+              Last Name
             </Label>
-            <Col md='10'>
-            
+            <Col md="10">
+              <Field name='lastName' placeholder='Last Name' className="form-control" />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label htmlFor='phoneNum' md='2'>
-                Phone
+            <Label htmlFor="phoneNum" md="2">
+              Phone
             </Label>
-            <Col md='10'>
-            
+            <Col md="10">
+              <Field name='phoneNum' placeholder='Phone' className="form-control" />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label htmlFor='email' md='2'>
-                Email
+            <Label htmlFor="email" md="2">
+              Email
             </Label>
-            <Col md='10'>
-            
+            <Col md="10">
+              <Field name='email' placeholder='Email' type='email' className="form-control" />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label check md={{size: 4, offset: 2 }}>
-                May we contact you?
+            <Label check md={{ size: 4, offset: 2 }}>
+                <Field name='agree' type='checkbox' className='form-check-input' />{' '}
+              May we contact you?
             </Label>
-            <Col md='4'>
-            
+            <Col md="4">
+              <Field name='contactType' as='select' className="form-control">
+                    <option>By Phone</option>
+                    <option>By Email</option>
+              </Field>
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Label htmlFor='feedback' md='2'>
-                Your Feedback
+            <Label htmlFor="feedback" md="2">
+              Your Feedback
             </Label>
-            <Col md='10'>
-            
+            <Col md="10">
+              <Field name='feedback' as='textarea' rows='12' className="form-control" />
             </Col>
           </FormGroup>
           <FormGroup row></FormGroup>
