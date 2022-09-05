936719f5c45d3ca2e57c8586d9ecef52a709cd87
Developer-AaronB <91814805+Developer-AaronB@users.noreply.github.com>
2022-09-04 20:47:55 -0500

    Async Campsites/ Error and loading

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
@@ -6,9 +8,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import "./App.css";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
+
   return (
     <div className="App">
       <Header />
--- src/pages/CampsiteDetailPage.js
+++ src/pages/CampsiteDetailPage.js
@@ -5,16 +5,35 @@ import { selectCampsiteBySlug } from "../features/campsites/campsitesSlice";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CommentsList from "../features/comments/CommentsList";
 import SubHeader from "../components/SubHeader";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
 
 const CampsiteDetailPage = () => {
   const { slug } = useParams();
   const campsite = useSelector(selectCampsiteBySlug(slug));
-  return (
-    <Container>
-      <SubHeader current={campsite.name} detail={true} />
-      <Row>
+
+  const isLoading = useSelector((state) => state.campsites.isLoading);
+  const errMsg = useSelector((state) => state.campsites.errMsg);
+  let content = null;
+
+  if (isLoading) {
+    content = <Loading />;
+  } else if (errMsg) {
+    content = <Error errMsg={errMsg} />
+  } else {
+    content = (
+      <>
         <CampsiteDetail campsite={campsite} />
         <CommentsList campsiteId={campsite.id} />
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      {campsite && <SubHeader current={campsite.name} detail={true} />}
+      <Row>
+        {content}
       </Row>
     </Container>
   );
